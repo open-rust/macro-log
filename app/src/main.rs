68d3bfe20a62230bfb365c61043c464860425525
develon2015 <302615249@qq.com>
2023-08-09 11:01:50 +0800

    土法炮制

--- app/src/main.rs
+++ app/src/main.rs
@@ -2,8 +2,18 @@ use macro_log::*;
 
 #[attr_test]
 fn main() {
+    test(2147483647);
 }
 
+#[attr_test]
+fn test(value: i32) {
+    println!("test run, value is: {:X}", value);
+    // `format_args!` cannot capture variables when the format string is expanded from a macro
+    // println!("test run, value is: {value:X}"); // error: there is no argument named `value`
+    println!("test ok");
+}
+
+#[cfg(test)]
 mod tests {
     use macro_log::*;
 
--- macro-log-macros/src/lib.rs
+++ macro-log-macros/src/lib.rs
@@ -1,4 +1,4 @@
-use proc_macro::TokenStream;
+use proc_macro::{TokenStream, TokenTree};
 
 // 类函数宏
 #[proc_macro]
@@ -12,5 +12,22 @@ pub fn make_compile_time_fn(item: TokenStream) -> TokenStream {
 pub fn attr_test(args: TokenStream, item: TokenStream) -> TokenStream {
     println!("attr_test args: {:?}", args);
     println!("attr_test item: {:?}", item);
-    todo!()
+    for token in item.clone().into_iter() {
+        println!("token -> {token} {:?}", token.span());
+    }
+    let mut new_fn = item.clone().into_iter().collect::<Vec<TokenTree>>();
+    let body = new_fn.pop().unwrap();
+    let new_stream = r#"
+        $fn {
+            macro_log::i!("This is new $fn");
+            $body
+        }
+    "#
+        .replace("$fn", TokenStream::from_iter(new_fn.clone()).to_string().as_str())
+        .replace("$body", body.to_string().as_str())
+        .parse::<TokenStream>().unwrap();
+    println!("stream: {}", new_stream);
+    return new_stream;
+    // new_fn.push(new_stream.into_iter().last().unwrap());
+    // return TokenStream::from_iter(new_fn);
 }
